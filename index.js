8380d7f01d83528220e713ff248f7376172c6be0
Anastasia Shuba <ashuba22@gmail.com>
2023-12-07 21:22:30 -0800

    add more permissions

--- index.js
+++ index.js
@@ -24,6 +24,45 @@ function requestMic() {
 }
 
 function listen() {
+  const perms = [
+    "geolocation",
+    "notifications",
+    "push",
+    "midi",
+    "camera",
+    "microphone",
+    "background-fetch",
+    "background-sync",
+    "persistent-storage",
+    "ambient-light-sensor",
+    "accelerometer",
+    "gyroscope",
+    "magnetometer",
+    "screen-wake-lock",
+    "nfc",
+    "display-capture",
+    "accessibility-events",
+    "clipboard-read",
+    "clipboard-write",
+    "payment-handler",
+    "idle-detection",
+    "periodic-background-sync",
+    "system-wake-lock",
+    "storage-access",
+    "window-management",
+    "window-placement",
+    "local-fonts",
+    "top-level-storage-access",
+  ];
+  
+  var selectElement = document.getElementById("mSelect");
+  for (const perm of perms) {
+    var option = document.createElement("option");
+    option.text = perm;
+    option.value = perm;
+    selectElement.add(option);
+  }
+
   document.querySelector('#request').addEventListener('click', clickHandler);
   document.querySelector('#camera').addEventListener('click', requestCamera);
   document.querySelector('#mic').addEventListener('click', requestMic);
